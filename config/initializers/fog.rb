09349f51e616ead8ed506049ae95e5f0041af90c
Derrick <jnhn333@gmail.com>
2017-06-15 17:55:54 +0000

    another commit

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     'AKIAJO37ZUMDXGTX33QA',                        # required
     aws_secret_access_key: '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',                        # required
-    region:                'ap-northeast-2',             # optional, defaults to 'us-east-1'
+    region:                'ap-northeast-2'            # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'using-heroku'            # required
 end

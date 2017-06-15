0d524467c6c989172e39d790af12f5ac24199e76
Derrick <jnhn333@gmail.com>
2017-06-15 18:36:01 +0000

    another commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = ENV['using-heroku']
   config.aws_acl    = :public_read
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/'
+  config.asset_host = 'https://using-heroku.s3-ap-northeast-1.amazonaws.com/'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
   config.aws_credentials = {
